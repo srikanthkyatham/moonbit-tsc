a615b6d149ad68ea8dd69127c5722b3b67a86a0e
Srikanth Kyatham <srikanth.1.kyatham@gmail.com>
2025-11-29 14:39:48 +0200

    feat: add import X = M.N, export import X = N  namespace alias support

---
+++ src/moonbit/e2e_tests/baselines/reference/acceptableAlias1.js
@@ -0,0 +1,15 @@
+//// [tests/cases/compiler/acceptableAlias1.ts] ////
+
+//// [acceptableAlias1.ts]
+namespace M {
+    export namespace N {
+    }
+    export import X = N;
+}
+
+import r = M.X;
+
+//// [acceptableAlias1.js]
+var M;
+(function (M) {
+})(M || (M = {}));
